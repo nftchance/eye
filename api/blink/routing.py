f8cc9e2e609433ac4217ec8f3e34740ce3531103
chance <chance@utc24.io>
2022-11-19 21:26:29 -0600

    fix: updating routes

--- api/blink/routing.py
+++ api/blink/routing.py
@@ -6,8 +6,6 @@ from .consumers import (
 )
 
 websocket_urlpatterns = [
-    path("ws/eye/<eye_id>/", EyeConsumer.as_asgi()),
-    path("ws/eye/<eye_id>/", EyeConsumer.as_asgi()),
-    path("ws/eye/<eye_id>/blink/", BlinkConsumer.as_asgi()),
-    path("ws/eye/<eye_id>/blink/<blink_id>/", BlinkConsumer.as_asgi())
+    path("ws/eye/", EyeConsumer.as_asgi()),
+    path("ws/eye/<eye_id>/blink", BlinkConsumer.as_asgi())
 ]
